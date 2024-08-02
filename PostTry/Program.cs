23265b83efbe493f1e0ca353b35e6950b647c426
HarrRob <144249299+HarrRob@users.noreply.github.com>
2024-08-02 03:31:41 +0100

    Add project files.

---
+++ PostTry/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PostTry
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            TokenService tokenService = new TokenService();
+            string token = await tokenService.GetAccessTokenAsync();
+            var getReccomendations = new GetReccomendations();
+            var recommendations = await getReccomendations.GetRecommendationsAsync(token, seedArtists: "artist_id1,artist_id2", seedTracks: "track_id1,track_id2");
+
+            Console.WriteLine("Recommendations:");
+            Console.WriteLine(recommendations.ToString());
+        }
+    }
+}
---
+++ PostTry/TokenService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace PostTry
+{
+    internal class TokenService
+    {
+        public async Task<string> GetAccessTokenAsync()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://accounts.spotify.com/api/token");
+
+            request.Headers.Add("Authorization", "Basic ZjlhMThlYTJhZTE1NDI2YTg2NjVkNGRmOTJmZWI0MTg6MGVjOTcwZTY1ZjgxNDI2YmFhNDhjZGFjMWQ2NzgwYjI=");
+            request.Headers.Add("Cookie", "__Host-device_id=AQC0HksgOqqtxnMGLBTi-z7OoDCSPfQQCrCUti7ddhM4WVpvWRiX929YdG07df-xlazsaTJkrKWm3fFenQXvIDUJIpSfpisKGSk");
+
+            var collection = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("grant_type", "client_credentials")
+            };
+
+            var content = new FormUrlEncodedContent(collection);
+            request.Content = content;
+
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseContent);
+
+            // Parse the JSON response to extract the access token
+            var json = JObject.Parse(responseContent);
+            var accessToken = json["access_token"]?.ToString();
+
+            Console.WriteLine("Access Token: " + accessToken);
+
+            return accessToken;
+        }
+    }
+}
